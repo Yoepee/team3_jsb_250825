6dbe89bdcf02f8db0e139452d58692ff7539a828
leejiyeon <sky75013@naver.com>
2025-08-26 17:13:40 +0900

    feat: 질문 등록 폼

--- src/main/java/com/back/domain/question/question/controller/QuestionController.java
+++ src/main/java/com/back/domain/question/question/controller/QuestionController.java
@@ -3,9 +3,17 @@ package com.back.domain.question.question.controller;
 import com.back.domain.question.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question/question/question_form";
+    }
 }
