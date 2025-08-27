f0dd874538de0ef231129fa2f99c09f7d51b0f6f
leejiyeon <sky75013@naver.com>
2025-08-28 01:45:21 +0900

    refactor: 질문 등록 폼 DTO 클래스 분리

--- src/main/java/com/back/domain/question/question/controller/QuestionController.java
+++ src/main/java/com/back/domain/question/question/controller/QuestionController.java
@@ -1,18 +1,18 @@
 package com.back.domain.question.question.controller;
 
 import com.back.domain.question.question.entity.Question;
+import com.back.domain.question.question.model.QuestionForm;
 import com.back.domain.question.question.service.QuestionService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/question")
 @Controller
@@ -21,25 +21,13 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/create")
-    public String showCreate(@ModelAttribute("questionForm") QuestionForm form) {
+    public String showCreate(@ModelAttribute("form") QuestionForm form) {
         return "question/question/question_form";
-    }
-
-    @AllArgsConstructor
-    @Getter
-    public static class QuestionForm {
-        @NotBlank(message = "주제를 입력해주세요.")
-        @Size(min = 1, max = 20, message = "제목은 1~10 자 이내로 입력해주세요.")
-        String subject;
-
-        @NotBlank(message = "내용을 입력해주세요.")
-        @Size(min = 2, max = 100, message = "내용은 2~100 자 이내로 입력해주세요.")
-        String content;
     }
 
     @PostMapping("/create")
     @Transactional
-    public String create(@ModelAttribute("questionForm") @Valid QuestionForm form,
+    public String create(@ModelAttribute("form") @Valid QuestionForm form,
                          BindingResult bindingResult,
                          Model model) {
         if(bindingResult.hasErrors()) {
