632f8c8dcb583317c618cee0737a3e0dd13e32c9
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-08-27 10:09:30 +0900

    [feat] 회원가입, 로그인 기능 구현

--- src/main/java/com/back/domain/member/member/controller/MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/MemberController.java
@@ -1,11 +1,43 @@
 package com.back.domain.member.member.controller;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "member/signin";
+    }
+
+    @PostMapping("/signin")
+    public String login(String username, String password) {
+        return memberService.signin(username, password) ? "redirect:/question" : "redirect:/signin";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "member/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(String username, String password, String nickname) {
+        Member member = new Member(username, password, nickname);
+        if (memberService.memberExist(username) == null) {
+            memberService.save(member);
+            return "redirect:/signin";
+        }
+        return "redirect:/signup";
+    }
+
+
 }
--- src/main/java/com/back/domain/member/member/service/MemberService.java
+++ src/main/java/com/back/domain/member/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.back.domain.member.member.service;
 
+import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    public boolean signin(String username, String password) {
+        Member findMember = memberExist(username);
+        if (findMember == null) return false;
+        else return findMember.getPassword().equals(password);
+    }
+
+    public void save(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Member memberExist(String username) {
+        return memberRepository.findByUsername(username).orElse(null);
+    }
 }
