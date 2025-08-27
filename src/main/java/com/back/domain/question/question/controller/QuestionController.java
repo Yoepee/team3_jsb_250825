1f3ecdae0c0f771b3ea3ec516679cef3eaea0190
leejiyeon <sky75013@naver.com>
2025-08-27 16:53:23 +0900

    feat: 질문 등록 Validation

--- src/main/java/com/back/domain/question/question/controller/QuestionController.java
+++ src/main/java/com/back/domain/question/question/controller/QuestionController.java
@@ -2,9 +2,16 @@ package com.back.domain.question.question.controller;
 
 import com.back.domain.question.question.entity.Question;
 import com.back.domain.question.question.service.QuestionService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/question")
@@ -14,23 +21,34 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String showCreate(@ModelAttribute("questionForm") QuestionForm form) {
         return "question/question/question_form";
     }
 
+    @AllArgsConstructor
+    @Getter
+    public static class QuestionForm {
+        @NotBlank(message = "주제를 입력해주세요.")
+        @Size(min = 1, max = 20, message = "제목은 1~10 자 이내로 입력해주세요.")
+        String subject;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Size(min = 2, max = 100, message = "내용은 2~100 자 이내로 입력해주세요.")
+        String content;
+    }
+
     @PostMapping("/create")
-    @ResponseBody
     @Transactional
-    public String questionCreate(@RequestParam(value = "subject") String subject, @RequestParam(value = "content") String content) {
-        Question question = questionService.create(subject, content);
-        return """
-                <div>
-                    <span>%d</span>
-                    /
-                    <span>%s</span>
-                    /
-                    <span>%s</span>
-                </div>
-                """.formatted(question.getId(), question.getSubject(), question.getContent()); // TODO 질문 상세 보기로 이동
+    public String create(@ModelAttribute("questionForm") @Valid QuestionForm form,
+                         BindingResult bindingResult,
+                         Model model) {
+        if(bindingResult.hasErrors()) {
+            return "question/question/question_form";
+        }
+
+        Question question = questionService.create(form.getSubject(), form.getContent());
+        model.addAttribute("question", question);
+
+        return "question/question/question_form"; // TODO 질문 상세 보기로 이동
     }
 }
